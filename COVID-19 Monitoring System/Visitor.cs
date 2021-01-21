fb2b33acc9ea25eb24d3ee0ea919d27f2bdf2b62
Chen Han <s10202961@connect.np.edu.sg>
2021-01-21 15:59:37 +0800

    Edited Person, Resident and Vistors class

--- COVID-19 Monitoring System/Visitor.cs
+++ COVID-19 Monitoring System/Visitor.cs
@@ -8,9 +8,44 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+//CalculateSHNCharges() method
 namespace COVID_19_Monitoring_System
 {
-    class Visitor
+    class Visitor: Person
     {
+        private string passportNo;
+
+        public string PassportNo
+        {
+            get { return passportNo; }
+            set { passportNo = value; }
+        }
+
+        private string nationality;
+
+        public string Nationality
+        {
+            get { return nationality; }
+            set { nationality = value; }
+        }
+
+        public Visitor(string p, string na, string n):base(n)
+        {
+            PassportNo = p;
+            Nationality = na;
+            
+        }
+
+        public override double CalculateSHNCharges()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return "Passport No: " + PassportNo + "\nNationality: " + Nationality + "Name: " + Name;
+        }
+
+
     }
 }
