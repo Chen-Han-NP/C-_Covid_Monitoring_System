76f78e2535f6ca832fe6854f54a753712852f7c9
TzeSiong <s10204388@connect.np.edu.sg>
2021-01-21 15:47:25 +0800

    Added attributes and methods to SHNFacility and TravelEntry classes.

--- COVID-19 Monitoring System/SHNFacility.cs
+++ COVID-19 Monitoring System/SHNFacility.cs
@@ -8,9 +8,96 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+//CalculateTravelCost()
 namespace COVID_19_Monitoring_System
 {
     class SHNFacility
     {
+
+        //attributes
+        private string facilityName;
+
+        public string FacilityName
+        {
+            get { return facilityName; }
+            set { facilityName = value; }
+        }
+
+        private int facilityCapacity;
+
+        public int FacilityCapacity
+        {
+            get { return facilityCapacity; }
+            set { facilityCapacity = value; }
+        }
+
+        private int facilityVacancy;
+
+        public int FacilityVacancy
+        {
+            get { return facilityVacancy; }
+            set { facilityVacancy = value; }
+        }
+
+        private double distFromAirCheckpoint;
+
+        public double DistFromAirCheckpoint
+        {
+            get { return distFromAirCheckpoint; }
+            set { distFromAirCheckpoint = value; }
+        }
+
+        private double distFromSeaCheckpoint;
+
+        public double DistFromSeaCheckpoint
+        {
+            get { return distFromSeaCheckpoint; }
+            set { distFromSeaCheckpoint = value; }
+        }
+
+        private double distFromLandCheckpoint;
+
+        public double DistFromLandCheckpoint
+        {
+            get { return distFromLandCheckpoint; }
+            set { distFromLandCheckpoint = value; }
+        }
+
+        //methods
+        public SHNFacility() { }
+
+        public SHNFacility(string fn, int fc, double ac, double sc, double lc)
+        {
+            FacilityName = fn;
+            FacilityCapacity = fc;
+            DistFromAirCheckpoint = ac;
+            DistFromSeaCheckpoint = sc;
+            DistFromLandCheckpoint = lc;
+        }
+
+        //Add string, DateTime
+        public double CalculateTravelCost()
+        {
+            return 0;
+        }
+
+        public bool IsAvailable()
+        {
+            if(FacilityVacancy > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "Facility Name: " + FacilityName + "\tFacility Capacity: " + FacilityCapacity + "\tFacility Vacancy: " + FacilityVacancy + 
+                "\tDistance from Air Checkpoint: " + DistFromAirCheckpoint + "\tDistance from Sea Checkpoint: " + DistFromSeaCheckpoint + "\tDistance from Land Checkpoint: " + DistFromLandCheckpoint;
+        }
+
     }
 }
--- COVID-19 Monitoring System/TravelEntry.cs
+++ COVID-19 Monitoring System/TravelEntry.cs
@@ -8,9 +8,83 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+//AssignFacility(), CalculateSHNDuration()
 namespace COVID_19_Monitoring_System
 {
     class TravelEntry
     {
+        private string lastCountryOfEmbarkation;
+
+        public string LastCoutryOfEmbarkation
+        {
+            get { return lastCountryOfEmbarkation; }
+            set { lastCountryOfEmbarkation = value; }
+        }
+
+        private string entryMode;
+
+        public string EntryMode
+        {
+            get { return entryMode; }
+            set { entryMode = value; }
+        }
+
+        private DateTime entryDate;
+
+        public DateTime EntryDate
+        {
+            get { return entryDate; }
+            set { entryDate = value; }
+        }
+
+        private DateTime shnEndDate;
+
+        public DateTime ShnEndDate
+        {
+            get { return shnEndDate; }
+            set { shnEndDate = value; }
+        }
+
+        private SHNFacility shnStay;
+
+        public SHNFacility ShnStay
+        {
+            get { return shnStay; }
+            set { shnStay = value; }
+        }
+
+        private bool isPaid;
+
+        public bool IsPaid
+        {
+            get { return isPaid; }
+            set { isPaid = value; }
+        }
+
+        //methods
+        public TravelEntry() { }
+
+        public TravelEntry(string lcoe, string em, DateTime ed)
+        {
+            LastCoutryOfEmbarkation = lcoe;
+            EntryMode = em;
+            EntryDate = ed;
+        }
+
+        public void AssignSHNFacility(SHNFacility shn)
+        {
+            shn = new SHNFacility();
+        }
+
+        public void CalculateSHNDuration()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "Last Country of Embarkation: " + LastCoutryOfEmbarkation + "\tEntry Mode: " + EntryMode + "\tEntry Date: " + EntryDate + "\tSHN End Date: " + ShnStay;
+        }
+
     }
 }
