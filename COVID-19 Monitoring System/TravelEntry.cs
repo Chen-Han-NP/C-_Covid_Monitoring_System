97550d5830c245d7758e86fa798982bfc70ce02b
TzeSiong <s10204388@connect.np.edu.sg>
2021-01-21 15:04:25 +0800

    Added 7 more classes

---
+++ COVID-19 Monitoring System/TravelEntry.cs
@@ -0,0 +1,16 @@
+﻿//============================================================
+// Student Number : S10202961, S10204388
+// Student Name : Chen Han, Chung Tze Siong
+// Module Group : T04
+//============================================================
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COVID_19_Monitoring_System
+{
+    class TravelEntry
+    {
+    }
+}
