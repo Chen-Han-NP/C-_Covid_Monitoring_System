7794f86b85edd6f2237f0706d94100ad240347a2
Chen Han <s10202961@connect.np.edu.sg>
2021-01-21 15:00:10 +0800

    Added name and class on Program and Person.cs

--- COVID-19 Monitoring System/Person.cs
+++ COVID-19 Monitoring System/Person.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//============================================================
+// Student Number : S10202961, S10204388
+// Student Name : Chen Han, Chung Tze Siong
+// Module Group : T04
+//============================================================
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
--- COVID-19 Monitoring System/Program.cs
+++ COVID-19 Monitoring System/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//============================================================
+// Student Number : S10202961, S10204388
+// Student Name : Chen Han, Chung Tze Siong
+// Module Group : T04
+//============================================================
+
+using System;
+using System.Collections.Generic;
 
 namespace COVID_19_Monitoring_System
 {
@@ -7,6 +14,7 @@ namespace COVID_19_Monitoring_System
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
 }
