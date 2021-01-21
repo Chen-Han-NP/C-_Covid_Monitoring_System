0775cabda5987220ad7fd15b0f5c6966514cc5b6
Chen Han <s10202961@connect.np.edu.sg>
2021-01-21 14:48:00 +0800

    Add project files.

---
+++ COVID-19 Monitoring System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COVID_19_Monitoring_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
