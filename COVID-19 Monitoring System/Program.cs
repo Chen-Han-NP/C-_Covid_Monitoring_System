135b31dc98c74e31bd02db62a90b7ec02f92c57d
Chen Han <s10202961@connect.np.edu.sg>
2021-01-22 21:27:41 +0800

    Edited Program.cs, completed Task 1.1 and task 2.1

--- COVID-19 Monitoring System/Program.cs
+++ COVID-19 Monitoring System/Program.cs
@@ -14,15 +14,15 @@ using Newtonsoft.Json;
 
 /* TO DO 
 1) Load Person and Business Location Data
-1. load both given CSV and populate two lists
+    1. load both given CSV and populate two lists
 2) Load SHN Facility Data
-1. call API and populate a list
+    1. call API and populate a list
 3) List all Visitors
 4) List Person Details
-1. prompt user for name
-2. search for person
-3. list person details including TravelEntry and SafeEntry details
-i. if resident, display TraceTogetherToken details
+    1. prompt user for name
+    2. search for person
+    3. list person details including TravelEntry and SafeEntry details
+        i. if resident, display TraceTogetherToken details
 */
 
 
@@ -32,14 +32,23 @@ namespace COVID_19_Monitoring_System
     {
         static void Main(string[] args)
         {
+            //Task 1.1
+            displayPersonData();
+            displayBusinessLocation();
+
+
+            //Make a list to store Resident and Visitor
             List<Person> personList = new List<Person>();
+
 
             using (StreamReader sr = new StreamReader("Person.csv"))
             {
                 string s = sr.ReadLine();
+
                 while ((s = sr.ReadLine()) != null)
                 {
                     string[] items = s.Split(',');
+
                     //Check the type of the Person, either visitor or resident
                     if (items[0] == "resident")
                     {
@@ -63,7 +72,7 @@ namespace COVID_19_Monitoring_System
                                 }
                             }
                         }
-                        
+
 
 
                     }
@@ -72,6 +81,7 @@ namespace COVID_19_Monitoring_System
 
         }
 
+        //Task 2.1
         //Make a methdo that calls the API and search for the detail of the SHN facility using the name
         static List<SHNFacility> getSHNFacilityDetail()
         {
@@ -102,6 +112,46 @@ namespace COVID_19_Monitoring_System
 
         }
 
+        static void displayPersonData()
+        {
+            using (StreamReader sr = new StreamReader("Person.csv"))
+            {
+                string s = sr.ReadLine();
+                string[] headers = s.Split(',');
 
+                Console.WriteLine("Person.csv");
+                Console.WriteLine("{0, -8} {1, -8} {2, -20} {3, -16} {4, -11} {5, -12} {6, -11} {7, -25} {8, -15} {9, -25} {10, -15} {11, -16} {12, -16} {13, -20}",
+                    headers[0], headers[1], headers[2], headers[3], headers[4], headers[5], headers[6],
+                    headers[7], headers[8], headers[9], headers[10], headers[11], headers[12], headers[13]);
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    //Task 1.1
+                    string[] items = s.Split(',');
+                    Console.WriteLine("{0, -8} {1, -8} {2, -20} {3, -16} {4, -11} {5, -12} {6, -11} {7, -25} {8, -15} {9, -25} {10, -15} {11, -16} {12, -16} {13, -20}",
+                        items[0], items[1], items[2], items[3], items[4], items[5], items[6],
+                        items[7], items[8], items[9], items[10], items[11], items[12], items[13]);
+                }
+            }
+        }
+
+        static void displayBusinessLocation()
+        {
+            using (StreamReader sr = new StreamReader("BusinessLocation.csv"))
+            {
+                string s = sr.ReadLine();
+                string[] headers = s.Split(',');
+
+                Console.WriteLine("\nBusinessLocation.csv");
+                Console.WriteLine("{0, -25} {1, -14} {2, -20}", headers[0], headers[1], headers[2]);
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    //Task 1.1
+                    string[] items = s.Split(',');
+                    Console.WriteLine("{0, -25} {1, -14} {2, -20}", items[0], items[1], items[2]);
+                }
+            }
+        }
     }
 }
